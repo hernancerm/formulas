5e9a1f04ef655e261194a44d92e4455e24ab2111
Hernán Cervera <hjcerveram@hotmail.com>
2025-08-16 21:12:18 -0600

    chore(git-timeline): support macos x86

--- Formula/git-timeline.rb
+++ Formula/git-timeline.rb
@@ -1,18 +1,20 @@
 class GitTimeline < Formula
-  desc "A small wrapper for git-log which improves readability."
+  desc "Git log wrapper improving readability."
   homepage "https://github.com/hernancerm/git-timeline"
   version "2.0"
-
-  # Binaries are not provided in the GitHub release page for anything other than macOS arm64.
-  # When binaries are provided there for other platforms, then this can be removed.
-  depends_on :macos
-  depends_on arch: :arm64
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/hernancerm/git-timeline/releases/download/2.0/git-timeline-macos-arm64.tar.gz"
       sha256 "28b561ce14a4b88043f3b350c707cf2990c80048ce625ad8ff353003e61b75d1"
+    elsif
+      url "https://github.com/hernancerm/git-timeline/releases/download/2.0/git-timeline-macos-x86.tar.gz"
+      sha256 "f884a9da676c165f0dab024c53e9d9ef00ce59d6a1184c4f44c05cc7380f9a6a"
     end
+  end
+
+  on_linux do
+    odie "No pre-compiled binaries. Build on your own or use the uber JAR in the release: https://github.com/hernancerm/git-timeline"
   end
 
   def install
@@ -23,4 +25,3 @@ class GitTimeline < Formula
     system "git-timeline", "--version"
   end
 end
-
