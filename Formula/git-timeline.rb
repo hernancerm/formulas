e0fb1d0a14cee6180a789cb0f0ab6eafe15a6528
Hernán Cervera <hjcerveram@hotmail.com>
2026-09-13 22:26:04 -0600

    chore(git-timeline): change arm hardware check

--- Formula/git-timeline.rb
+++ Formula/git-timeline.rb
@@ -1,13 +1,15 @@
 class GitTimeline < Formula
-  desc "Git log wrapper improving readability."
+  desc "Git log wrapper improving readability"
   homepage "https://github.com/hernancerm/git-timeline"
   version "2.1"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/hernancerm/git-timeline/releases/download/2.1/git-timeline-macos-arm64.tar.gz"
       sha256 "1077302c0e16d37ea2aaee22728b62aae7c4e2cbbf9c07bee09af0289444f9cd"
-    elsif
+    end
+
+    on_intel do
       url "https://github.com/hernancerm/git-timeline/releases/download/2.1/git-timeline-macos-x86.tar.gz"
       sha256 "2b4953cabe544015d7c652c6b29ecae27258a6bed2166778fb09ecaf60fae23f"
     end
